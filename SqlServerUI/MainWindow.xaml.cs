b91cd34120d368424036d829c3d8ba1b3e0fee22
Amina <yusufovamina@gmail.com>
2024-01-12 22:48:54 +0400

    Add project files.

---
+++ SqlServerUI/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlServerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void connection_OpenClose(object sender, System.Data.StateChangeEventArgs e)
+        {
+            SqlConnection connection = sender as SqlConnection;
+
+
+            ServerNameTextBlock.Text = connection.DataSource;
+            DBNameTextBlock.Text = connection.Database;
+                //"State: " + connection.State + "\n"
+                
+        }
+        public void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text != "admin" && Password.Text != "admin")
+            {
+                MessageBox.Show("Incorrect username or password!");
+
+            }
+            else
+            {
+                string conStr = @"Data Source = DESKTOP-0LP9EBH; Initial Catalog = Hospital; Integrated Security = true;";
+                ellipse.Fill = Brushes.Green;
+                StateLabel.Content = "Connected";
+                SqlConnection connection = new SqlConnection(conStr);
+
+
+                connection.StateChange += connection_OpenClose;
+
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            } }
+
+        public void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            ellipse.Fill = Brushes.Red;
+            StateLabel.Content = "Disconnected";
+
+        }
+
+        
+    }
+}
